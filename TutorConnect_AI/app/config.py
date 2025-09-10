a846e02d102c72bd04a54021fae60aa8d51bc5d1
S. M. SAIDUR RAHMAN <22101451@bracu.ac.bd>
2025-09-10 12:59:49 +1200

    changed the LLM model

--- TutorConnect_AI/app/config.py
+++ TutorConnect_AI/app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     NOMIC_INFERENCE_MODE: str = 'local'  # set to 'remote' to use the API or 'local'
     GROQ_API_KEY: str
-    GROQ_MODEL: str = 'llama3-70b-8192'   # https://console.groq.com/docs/models
+    GROQ_MODEL: str = 'openai/gpt-oss-120b'     # https://console.groq.com/docs/models
 
     model_config = SettingsConfigDict(env_file='.env')
 
